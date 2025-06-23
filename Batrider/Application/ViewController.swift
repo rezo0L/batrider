1d9a022f51789268308ca0bc891ad009ace83641
Reza <reza.daneshvar@majority.com>
2025-06-23 19:23:57 +0200

    3. Introduce QRCodeScanner package

--- Batrider/Application/ViewController.swift
+++ Batrider/Application/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  Batrider
-//
-//  Created by Reza on 2025-06-23.
-//
-
+import QRCodeScanner
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let scannerView = QRCodeScannerPreviewView()
+        scannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scannerView)
+        NSLayoutConstraint.activate([
+            scannerView.topAnchor.constraint(equalTo: view.topAnchor),
+            scannerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scannerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scannerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        do {
+            try scannerView.startScanning()
+            scannerView.onCodeScanned = { code in
+                print("Scanned QR Code: \(code)")
+            }
+        } catch {
+            print("Failed to start scanning: \(error)")
+        }
     }
-
-
 }
-
