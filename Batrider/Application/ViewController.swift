b050883fd969f2be6f2646b12d48e79d240cd3d0
Reza <reza.daneshvar@majority.com>
2025-06-23 17:56:32 +0200

    1. Create the Xcode project

---
+++ Batrider/Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Batrider
+//
+//  Created by Reza on 2025-06-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
