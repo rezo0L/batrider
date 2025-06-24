5490dcbf82fa270d9341b01cfc83848e5433eefe
Reza <reza.daneshvar@majority.com>
2025-06-24 16:58:47 +0200

    3. Extend QRCodeScanner package

--- Batrider/Utilities/QRCodeScanner/Sources/QRCodeScanner/QRCodeScannerError.swift
+++ Batrider/Utilities/QRCodeScanner/Sources/QRCodeScanner/QRCodeScannerError.swift
@@ -1,6 +1,9 @@
 /// An error type representing failures that can occur when using a QR code scanner.
 public enum QRCodeScannerError: Error {
 
+    /// Indicates that the camera permission was not granted.
+    case notAuthorized
+
     /// Indicates that the device's camera is not available for use.
     case cameraUnavailable
 }
