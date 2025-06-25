e795ae5e9bb0c0aadd043ba0c7442ea5bde23eea
Reza <reza.daneshvar@majority.com>
2025-06-25 15:19:32 +0200

    23. Improve error handling by providing description

--- Batrider/Utilities/QRCodeScanner/Sources/QRCodeScanner/QRCodeScannerError.swift
+++ Batrider/Utilities/QRCodeScanner/Sources/QRCodeScanner/QRCodeScannerError.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 /// An error type representing failures that can occur when using a QR code scanner.
 public enum QRCodeScannerError: Error {
 
@@ -6,4 +8,15 @@ public enum QRCodeScannerError: Error {
 
     /// Indicates that the device's camera is not available for use.
     case cameraUnavailable
+}
+
+extension QRCodeScannerError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .notAuthorized:
+            return .cameraNotAuthorizedError
+        case .cameraUnavailable:
+            return .cameraUnavailableError
+        }
+    }
 }
